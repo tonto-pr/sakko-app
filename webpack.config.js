1d7e946ed4f6b33b6363e1cd91cd5b45a2492592
Anton Rautio <anton.rautio@smartly.io>
2020-03-05 18:47:41 +0200

    Fix HMR

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
     },
     output: {
         path: __dirname + '/dist',
-        publicPath: './',
+        publicPath: '/dist',
         filename: 'bundle.js'
     },
     plugins: [
