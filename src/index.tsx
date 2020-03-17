5a90fb0a3df345d9d0d30a0c196790eeaaee3538
Anton Rautio <antonrautio@Antons-MacBook-Pro.local>
2020-03-17 22:00:39 +0200

    Rename site

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Title from './components/Title.tsx';
 
-const title = 'React with Webpack and Babel';
-
 ReactDOM.render(
-  <Title message="Leivalle ;)" />,
+  <Title message="Sakko.app" />,
   document.getElementById('app')
 );
 
